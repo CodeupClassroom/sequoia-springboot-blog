23c2bcc50586430b1e7173baa31c3a04fc4678f3
fer <fernandoza@gmail.com>
2018-01-17 15:45:04 -0600

    Finish the lecture for views and fragments

--- src/main/java/com/codeup/sequoiaspringbootblog/HomeController.java
+++ src/main/java/com/codeup/sequoiaspringbootblog/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String index() {
-        return "<h1>Hello!</h1>";
+        return "home";
     }
 }
