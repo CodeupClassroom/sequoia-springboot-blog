c2a4229d485c2ce5dc30555b332157dd38528c93
Luis Montealegre <montealegreluis@gmail.com>
2018-01-19 11:49:37 -0600

    Add explanation on how to approach a new feature

--- src/main/java/com/codeup/sequoiaspringbootblog/PostsController.java
+++ src/main/java/com/codeup/sequoiaspringbootblog/PostsController.java
@@ -14,6 +14,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// Page to search posts
+// 1. Create a form in the navigation bar
+//    - Find the fragment and add a new one for the search form
+// 2. Create a controller action to perform the search
+//    - Create a SearchController
+//    - Method to search by a term (GetMapping /search)
+// 3. Write code to query the DB using the term sent by the user
+///   - SearchService, will have a method search(term) -> select from posts where title like %term%
+//    - probably start with an empty list, then query the DB
+// 4 Create/reuse a template to loop over the result of the search
+//    public String search(@PathVariable String term) {
+//     return 'post/index';  // instead of 'posts/search-result'
+//    }
+
 // Dependency injection
 // 1. Constructor injection (preferred) -> required dependencies
 // 2. Setter injection -> optional dependencies
