54cb67068f4d12e75085e6059d80ff570bb87433
Zach Gulde <zachgulde@gmail.com>
2018-01-23 11:37:36 -0600

    Allow application to be packaged and deployed as a war

--- src/main/java/com/codeup/sequoiaspringbootblog/SequoiaSpringbootBlogApplication.java
+++ src/main/java/com/codeup/sequoiaspringbootblog/SequoiaSpringbootBlogApplication.java
@@ -2,12 +2,17 @@ package com.codeup.sequoiaspringbootblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SequoiaSpringbootBlogApplication {
+public class SequoiaSpringbootBlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SequoiaSpringbootBlogApplication.class, args);
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SequoiaSpringbootBlogApplication.class);
+	}
 }
